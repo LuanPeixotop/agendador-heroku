e6971878ba671425d3e326874e9575248da93346
LuanPeixotop <luanpeixotopereira@gmail.com>
2017-08-11 20:21:12 -0300

    1.0.1 Corrigido Bug ao Recarregar Página

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var path = require('path');
-var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname));
+app.use('/static', express.static(__dirname + '/static'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+app.all('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('index.html', { root: __dirname });
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
